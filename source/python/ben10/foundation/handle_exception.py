478d838ca9bc5e7a4e08bb57f77bf42bf05a70fb
Alexandre Andrade <ama@esss.com.br>
2014-07-29 22:07:19 -0300

    Updating code.

--- source/python/ben10/foundation/handle_exception.py
+++ source/python/ben10/foundation/handle_exception.py
@@ -2,13 +2,11 @@
 This module contains utility functions for dealing with exceptions that should be handled in the
 application.
 '''
-
 from ben10.foundation import log
 from ben10.foundation.callback import Callback
+from contextlib import contextmanager
 
-
-
-# Callback for clients that want to hear about handled exceptions.
+#Callback for clients that want to hear about handled exceptions.
 on_exception_handled = Callback()
 
 LOG_ERRORS = True
@@ -26,8 +24,6 @@ def StartIgnoreHandleException():
     global _ignore_expected_exception
     _ignore_expected_exception += 1
 
-
-
 #===================================================================================================
 # EndIgnoreHandleException
 #===================================================================================================
@@ -39,27 +35,41 @@ def EndIgnoreHandleException():
     _ignore_expected_exception -= 1
 
 
+
+#===================================================================================================
+# IgnoringHandleException
+#===================================================================================================
+@contextmanager
+def IgnoringHandleException():
+    """
+    Ignores exception handling during context.
+    """
+    StartIgnoreHandleException()
+    yield
+    EndIgnoreHandleException()
+
+
 #===================================================================================================
 # HandleException
 #===================================================================================================
-def HandleException(msg):
+def HandleException(msg=''):
     '''
     Handles the current exception (in sys.exc_info()) without actually continuing its raise.
 
     It should be used when for some reason the current exception should not stop the current
     execution flow but should still be shown to the user and reported accordingly.
     '''
-    # Let listeners know about the exception (usually the test case)
+    #Let listeners know about the exception (usually the test case)
     on_exception_handled()
 
     if _ignore_expected_exception > 0:
-        return  # Don't log nor call excepthook.
+        return #Don't log nor call excepthook.
 
     if LOG_ERRORS:
-        # Log it
+        #Log it
         log.Exception(msg)
 
-    # And just call the except hook (and keep on going with the calls) -- in the application
-    # this should trigger the except dialog.
+    #And just call the except hook (and keep on going with the calls) -- in the application
+    #this should trigger the except dialog.
     import sys
     sys.excepthook(*sys.exc_info())
