10291f86761ea5bad3240df2f2b6d17d77260074
Bruno Oliveira <bruno@esss.com.br>
2014-10-06 15:35:32 -0300

    Changed pytest_fixture tests to functions as requested in code review
    ETK-706

--- source/python/ben10/_tests/pytest_fixtures.py
+++ source/python/ben10/_tests/pytest_fixtures.py
@@ -7,125 +7,116 @@ import os
 import pytest
 
 
-pytest_plugins = ["ben10.fixtures"]
+def testEmbedData(embed_data):
+    assert not os.path.isdir('data_fixtures__testEmbedData')
+
+    embed_data.CreateDataDir()
+
+    assert os.path.isdir('data_fixtures__testEmbedData')
+
+    # Checking if all contents of pytest_fixtures is present
+    assert os.path.isfile(embed_data.GetDataFilename('alpha.txt'))
+    assert os.path.isfile(embed_data.GetDataFilename('alpha.dist-12.0-win32.txt'))
+    assert os.path.isfile(embed_data.GetDataFilename('alpha.dist-12.0.txt'))
+
+    # Checking auxiliary functions
+    assert embed_data.GetDataDirectory() == 'data_fixtures__testEmbedData'
+    assert embed_data.GetDataFilename('alpha.txt') == 'data_fixtures__testEmbedData/alpha.txt'
+
+    assert embed_data.GetDataDirectory(absolute=True) \
+        == StandardizePath(os.path.abspath('data_fixtures__testEmbedData'))
+    assert embed_data.GetDataFilename('alpha.txt', absolute=True) \
+        == StandardizePath(os.path.abspath('data_fixtures__testEmbedData/alpha.txt'))
 
 
+def testEmbedDataExistingDataDir(embed_data):
+    # Create the directory manually (we must not use any embed_data functions or else the
+    # directory is created)
+    extra_txt = 'data_fixtures__testEmbedDataExistingDataDir/extra.txt'
+    CreateFile(extra_txt, 'This file will perish')
+    assert os.path.isfile(extra_txt)
 
-#===================================================================================================
-# Test
-#===================================================================================================
-class TestEmbedData(object):
-
-    def testEmbedData(self, embed_data):
-        assert not os.path.isdir('data_fixtures__testEmbedData')
-
-        embed_data.CreateDataDir()
-
-        assert os.path.isdir('data_fixtures__testEmbedData')
-
-        # Checking if all contents of pytest_fixtures is present
-        assert os.path.isfile(embed_data.GetDataFilename('alpha.txt'))
-        assert os.path.isfile(embed_data.GetDataFilename('alpha.dist-12.0-win32.txt'))
-        assert os.path.isfile(embed_data.GetDataFilename('alpha.dist-12.0.txt'))
-
-        # Checking auxiliary functions
-        assert embed_data.GetDataDirectory() == 'data_fixtures__testEmbedData'
-        assert embed_data.GetDataFilename('alpha.txt') == 'data_fixtures__testEmbedData/alpha.txt'
-
-        assert embed_data.GetDataDirectory(absolute=True) \
-            == StandardizePath(os.path.abspath('data_fixtures__testEmbedData'))
-        assert embed_data.GetDataFilename('alpha.txt', absolute=True) \
-            == StandardizePath(os.path.abspath('data_fixtures__testEmbedData/alpha.txt'))
+    # Calling CreateDataDir again will recreate the directory, deleting the old file
+    embed_data.CreateDataDir()
+    assert not os.path.isfile(extra_txt)
 
 
-    def testEmbedDataExistingDataDir(self, embed_data):
-        # Create the directory manually (we must not use any embed_data functions or else the
-        # directory is created)
-        extra_txt = 'data_fixtures__testEmbedDataExistingDataDir/extra.txt'
-        CreateFile(extra_txt, 'This file will perish')
-        assert os.path.isfile(extra_txt)
+def testEmbedDataAssertEqualFiles(embed_data):
+    CreateFile(embed_data.GetDataFilename('equal.txt'), 'This is alpha.txt')
+    embed_data.AssertEqualFiles(
+        'alpha.txt',
+        'equal.txt'
+    )
 
-        # Calling CreateDataDir again will recreate the directory, deleting the old file
-        embed_data.CreateDataDir()
-        assert not os.path.isfile(extra_txt)
-
-
-    def testEmbedDataAssertEqualFiles(self, embed_data):
-        CreateFile(embed_data.GetDataFilename('equal.txt'), 'This is alpha.txt')
+    CreateFile(embed_data.GetDataFilename('different.txt'), 'This is different.txt')
+    with pytest.raises(AssertionError) as e:
         embed_data.AssertEqualFiles(
             'alpha.txt',
-            'equal.txt'
+            'different.txt'
         )
-
-        CreateFile(embed_data.GetDataFilename('different.txt'), 'This is different.txt')
-        with pytest.raises(AssertionError) as e:
-            embed_data.AssertEqualFiles(
-                'alpha.txt',
-                'different.txt'
-            )
-        assert str(e.value) == Dedent(
-            '''
-            *** FILENAME: data_fixtures__testEmbedDataAssertEqualFiles/alpha.txt
-            ***\w
-
-            ---\w
-
-            ***************
-
-            *** 1 ****
-
-            ! This is alpha.txt
-            --- 1 ----
-
-            ! This is different.txt
-            '''.replace('\w', ' ')
-        )
-
-
-        with pytest.raises(MultipleFilesNotFound) as e:
-            embed_data.AssertEqualFiles(
-                'alpha.txt',
-                'missing.txt'
-            )
-
-        assert (
-            str(e.value)
-            == 'Files not found: '
-            'missing.txt,data_fixtures__testEmbedDataAssertEqualFiles/missing.txt'
-        )
-
-
-    def testNotOnFrozen(self, monkeypatch, embed_data):
+    assert str(e.value) == Dedent(
         '''
-        We fail to create data directory IF we are inside a generated executable (IsFrozen).
-        '''
-        was_frozen = IsFrozen()
-        try:
-            SetIsFrozen(True)
+        *** FILENAME: data_fixtures__testEmbedDataAssertEqualFiles/alpha.txt
+        ***\w
 
-            with pytest.raises(RuntimeError) as exception:
-                embed_data.CreateDataDir()
+        ---\w
 
-            assert \
-                '_EmbedDataFixture is not ready for execution inside an executable.' \
-                in str(exception)
-        finally:
-            SetIsFrozen(was_frozen)
+        ***************
+
+        *** 1 ****
+
+        ! This is alpha.txt
+        --- 1 ----
+
+        ! This is different.txt
+        '''.replace('\w', ' ')
+    )
 
 
-    def testEmbedDataFixture(self, request):
+    with pytest.raises(MultipleFilesNotFound) as e:
+        embed_data.AssertEqualFiles(
+            'alpha.txt',
+            'missing.txt'
+        )
+
+    assert (
+        str(e.value)
+        == 'Files not found: '
+        'missing.txt,data_fixtures__testEmbedDataAssertEqualFiles/missing.txt'
+    )
+
+
+def testNotOnFrozen(monkeypatch, embed_data):
+    '''
+    We fail to create data directory IF we are inside a generated executable (IsFrozen).
+    '''
+    was_frozen = IsFrozen()
+    try:
+        SetIsFrozen(True)
+
+        with pytest.raises(RuntimeError) as exception:
+            embed_data.CreateDataDir()
+
+        assert \
+            '_EmbedDataFixture is not ready for execution inside an executable.' \
+            in str(exception)
+    finally:
+        SetIsFrozen(was_frozen)
+
+
+def testEmbedDataFixture(request):
+    assert os.path.isdir('data_fixtures__testEmbedDataFixture') == False
+
+    try:
+        embed_data = _EmbedDataFixture(request)
         assert os.path.isdir('data_fixtures__testEmbedDataFixture') == False
 
-        try:
-            embed_data = _EmbedDataFixture(request)
-            assert os.path.isdir('data_fixtures__testEmbedDataFixture') == False
+        assert embed_data.GetDataDirectory() == 'data_fixtures__testEmbedDataFixture'
+        assert os.path.isdir('data_fixtures__testEmbedDataFixture') == True
+    finally:
+        embed_data.Finalizer()
 
-            assert embed_data.GetDataDirectory() == 'data_fixtures__testEmbedDataFixture'
-            assert os.path.isdir('data_fixtures__testEmbedDataFixture') == True
-        finally:
-            embed_data.Finalizer()
-
-        assert os.path.isdir('data_fixtures__testEmbedDataFixture') == False
+    assert os.path.isdir('data_fixtures__testEmbedDataFixture') == False
 
 
 @pytest.mark.parametrize('i', [0, 1])
