72bb4d686cdd67996647b9decb303220df3a1820
Alexandre Andrade <ama@esss.com.br>
2014-10-07 08:52:22 -0300

    Reorganizing filesystem. Incorporating FindFiles and friends. Removing ETK sync code.

--- source/python/ben10/filesystem/_filesystem.py
+++ source/python/ben10/filesystem/_filesystem.py
@@ -536,6 +536,57 @@ def CopyFilesX(file_mapping):
 
 
 #===================================================================================================
+# CopyFilesX
+#===================================================================================================
+def CopyFilesX(file_mapping):
+    '''
+    Copies files into directories, according to a file mapping
+
+    :param list(tuple(unicode,unicode)) file_mapping:
+        A list of mappings between the directory in the target and the source.
+        For syntax, @see: ExtendedPathMask
+
+    :rtype: list(tuple(unicode,unicode))
+    :returns:
+        List of files copied. (source_filename, target_filename)
+
+    .. seealso:: FTP LIMITATIONS at this module's doc for performance issues information
+    '''
+    # List files that match the mapping
+    files = []
+    for i_target_path, i_source_path_mask in file_mapping:
+        tree_recurse, flat_recurse, dirname, in_filters, out_filters = ExtendedPathMask.Split(i_source_path_mask)
+
+        _AssertIsLocal(dirname)
+
+        filenames = FindFiles(dirname, in_filters, out_filters, tree_recurse)
+        for i_source_filename in filenames:
+            if os.path.isdir(i_source_filename):
+                continue  # Do not copy dirs
+
+            i_target_filename = i_source_filename[len(dirname) + 1:]
+            if flat_recurse:
+                i_target_filename = os.path.basename(i_target_filename)
+            i_target_filename = os.path.join(i_target_path, i_target_filename)
+
+            files.append((
+                StandardizePath(i_source_filename),
+                StandardizePath(i_target_filename)
+            ))
+
+    # Copy files
+    for i_source_filename, i_target_filename in files:
+        # Create target dir if necessary
+        target_dir = os.path.dirname(i_target_filename)
+        CreateDirectory(target_dir)
+
+        CopyFile(i_source_filename, i_target_filename)
+
+    return files
+
+
+
+#===================================================================================================
 # IsFile
 #===================================================================================================
 def IsFile(path):
@@ -896,7 +947,7 @@ def GetFileLines(filename, encoding=None):
     return GetFileContents(filename, binary=False, encoding=encoding).split('\n')
 
 
-def OpenFile(filename, binary=False):
+def OpenFile(filename, binary=False, universal_newlines=True):
     '''
     Open a file and returns it.
     Consider the possibility of a remote file (HTTP, HTTPS, FTP)
@@ -906,9 +957,13 @@ def OpenFile(filename, binary=False):
 
     :param bool binary:
         If True returns the file as is, ignore any EOL conversion.
+        If set ignores univeral_newlines parameter.
 
-    :rtype: file
-    :returns:
+    :param bool universal_newlines:
+        Opens the file as a text file, but lines terminated by any EOL conversion will be converted
+        to '\n'.
+
+    :returns file:
         The open file, it must be closed by the caller
 
     @raise: FileNotFoundError
@@ -927,6 +982,8 @@ def OpenFile(filename, binary=False):
         mode = 'r'
         if binary:
             mode += 'b'
+        elif universal_newlines:
+            mode += 'U'
         return file(filename, mode)
 
     # Not local
@@ -1091,22 +1148,29 @@ def CreateFile(filename, contents, eol_style=EOL_STYLE_NATIVE, create_dir=True, 
 
 
 
-def ReplaceInFile(filename, old, new):
+def ReplaceInFile(filename, old, new, encoding=None):
     '''
     Replaces all occurrences of "old" by "new" in the given file.
 
-    :param str filename:
+    :param unicode filename:
         The name of the file.
-    :param str old:
+
+    :param unicode old:
         The string to search for.
-    :param str new:
+
+    :param unicode new:
         Replacement string.
-    :return str:
+
+    :param unicode encoding:
+        Target file's content encoding.
+        Defaults to sys.getdefaultencoding()
+
+    :return unicode:
         The new contents of the file.
     '''
     content = GetFileContents(filename)
     content = content.replace(old, new)
-    CreateFile(filename, content)
+    CreateFile(filename, content, encoding=encoding)
     return content
 
 
@@ -1370,7 +1434,6 @@ def GetMTime(path):
     _AssertIsLocal(path)
 
     if os.path.isdir(path):
-        from ._duplicates import FindFiles
         files = FindFiles(path)
 
         if len(files) > 0:
@@ -1604,3 +1667,169 @@ def _CallWindowsNetCommand(parameters):
     if stderrdata:
         raise OSError("Failed on call net.exe: %s" % stderrdata)
     return stdoutdata
+
+
+
+#===================================================================================================
+# ExtendedPathMask
+#===================================================================================================
+class ExtendedPathMask(object):
+    '''
+    This class is a place-holder for functions that handle the extended path mask.
+
+    Extended Path Mask
+    ------------------
+
+    The extended path mask is a file search path description used to find files based on the filename.
+    This extended path mask includes the following features:
+        - Recursive search (prefix with a "+" sign)
+        - The possibility of adding more than one filter to match files (separated by ";")
+        - The possibility of negate an mask (prefix the mask with "!").
+
+    The extended path mask has the following syntax:
+
+        [+|-]<path>/<filter>(;<filter>)*
+
+    Where:
+        + : recursive and copy-tree flag
+        - : recursive and copy-flat flag (copy files to the target directory with no tree structure)
+        <path> : a usual path, using '/' as separator
+        <filter> : A filename filter, as used in dir command:
+            Ex:
+                *.zip;*.rar
+                units.txt;*.ini
+                *.txt;!*-002.txt
+    '''
+
+
+    @classmethod
+    def Split(cls, extended_path_mask):
+        '''
+        Splits the given path into their components: recursive, dirname, in_filters and out_filters
+
+        :param str: extended_path_mask:
+            The "extended path mask" to split
+
+        :rtype: tuple(bool,bool,str,list(str),list(str))
+        :returns:
+            Returns the extended path 5 components:
+            - The tree-recurse flag
+            - The flat-recurse flag
+            - The actual path
+            - A list of masks to include
+            - A list of masks to exclude
+        '''
+        import os.path
+        r_tree_recurse = extended_path_mask[0] in '+-'
+        r_flat_recurse = extended_path_mask[0] in '-'
+
+        r_dirname, r_filters = os.path.split(extended_path_mask)
+        if r_tree_recurse:
+            r_dirname = r_dirname[1:]
+
+        filters = r_filters.split(';')
+        r_in_filters = [i for i in filters if not i.startswith('!')]
+        r_out_filters = [i[1:] for i in filters if i.startswith('!')]
+
+        return r_tree_recurse, r_flat_recurse, r_dirname, r_in_filters, r_out_filters
+
+
+
+#===================================================================================================
+# CheckForUpdate
+#===================================================================================================
+def CheckForUpdate(source, target):
+    '''
+    Checks if the given target filename should be re-generated because the source has changed.
+    :param  source:
+            the source filename.
+    :param  target:
+            the target filename.
+    @return:
+        True if the target is out-dated, False otherwise.
+    '''
+    return \
+        not os.path.isfile(target) or \
+        os.path.getmtime(source) > os.path.getmtime(target)
+
+
+
+#===================================================================================================
+# MatchMasks
+#===================================================================================================
+def MatchMasks(filename, filters):
+    '''
+    Verifies if a filename match with given patterns.
+
+    :type filename: the filename to match.
+    :param filename:
+    :type filters: the patterns to search in the filename.
+    :param filters:
+    :rtype: True if the filename has matched with one pattern, False otherwise.
+    '''
+    import fnmatch
+    if not isinstance(filters, (list, tuple)):
+        filters = [filters]
+
+    for i_filter in filters:
+        if fnmatch.fnmatch(filename, i_filter):
+            return True
+    return False
+
+
+
+#===================================================================================================
+# FindFiles
+#===================================================================================================
+def FindFiles(dir_, in_filters=None, out_filters=None, recursive=True, include_root_dir=True, standard_paths=False):
+    '''
+    Searches for files in a given directory that match with the given patterns.
+
+    :type dir_: the directory root, to search the files.
+    :param dir_:
+    :type in_filters: a list with patterns to match (default = all). E.g.: ['*.py']
+    :param in_filters:
+    :type out_filters: a list with patterns to ignore (default = none). E.g.: ['*.py']
+    :param out_filters:
+    :type recursive: if True search in subdirectories, otherwise, just in the root.
+    :param recursive:
+    :type include_root_dir: if True, includes the directory being searched in the returned paths
+    :param include_root_dir:
+    :type standard_paths: if True, always uses unix path separators "/"
+    :param standard_paths:
+    :rtype: a list of strings with the files that matched (with the full path in the filesystem).
+    '''
+    # all files
+    if in_filters is None:
+        in_filters = ['*']
+
+    if out_filters is None:
+        out_filters = []
+
+    result = []
+
+    # maintain just files that don't have a pattern that match with out_filters
+    # walk through all directories based on dir
+    for dir_root, directories, filenames in os.walk(dir_):
+
+        for i_directory in directories[:]:
+            if MatchMasks(i_directory, out_filters):
+                directories.remove(i_directory)
+
+        for filename in directories + filenames:
+            if MatchMasks(filename, in_filters) and not MatchMasks(filename, out_filters):
+                result.append(os.path.join(dir_root, filename))
+
+        if not recursive:
+            break
+
+    if not include_root_dir:
+        # Remove root dir from all paths
+        dir_prefix = len(dir_) + 1
+        result = [file[dir_prefix:] for file in result]
+
+    if standard_paths:
+        from ben10.filesystem import StandardizePath
+        result = map(StandardizePath, result)
+
+    return result
--- source/python/ben10/filesystem/_tests/pytest_filesystem.py
+++ source/python/ben10/filesystem/_tests/pytest_filesystem.py
@@ -8,7 +8,7 @@ from ben10.filesystem import (AppendToFile, CanonicalPath, CheckIsDir, CheckIsFi
     IsDir, IsFile, IsLink, ListFiles, ListMappedNetworkDrives, MD5_SKIP, MoveDirectory, MoveFile,
     NormStandardPath, NormalizePath, NotImplementedForRemotePathError, NotImplementedProtocol,
     OpenFile, ReadLink, ReplaceInFile, ServerTimeoutError, StandardizePath)
-from ben10.filesystem._filesystem import CreateTemporaryFile
+from ben10.filesystem._filesystem import CreateTemporaryFile, FindFiles
 from mock import patch
 import errno
 import logging
@@ -376,9 +376,11 @@ class Test:
 
         # Use OpenFile to obtain the file contents, with binary=False and binary=True
         iss = OpenFile(test_filename, binary=False)
+        assert iss.read() == 'Alpha\nBravo\nCharlie\nDelta'
 
-        expected = 'Alpha\nBravo\nCharlie\nDelta'
-        assert iss.read() == expected
+        # NOTE: Here it converts windows eol ('\r\n') to '\n' but not '\r' to '\n'.
+        iss = OpenFile(test_filename, universal_newlines=False)
+        assert iss.read() == 'Alpha\nBravo\nCharlie\rDelta'
 
         iss = OpenFile(test_filename, binary=True)
         assert iss.read() == 'Alpha\nBravo\r\nCharlie\rDelta'
@@ -1320,6 +1322,149 @@ class Test:
         assert mapped_drives[1][0] == 'O:'
         assert mapped_drives[1][2] == False
         assert mapped_drives[2][0] == 'P:'
+
+
+    def testFindFiles(self, embed_data):
+        def PATH(p_path):
+            return os.path.normpath(p_path)
+
+        def Compare(p_obtained, p_expected):
+            obtained = set(map(PATH, p_obtained))
+            expected = set(map(PATH, p_expected))
+            assert obtained == expected
+
+        CreateDirectory(embed_data['test_find_files/A/B'])
+        CreateDirectory(embed_data['test_find_files/A/C'])
+        CreateFile(embed_data['test_find_files/testRoot.bmp'], contents='')
+        CreateFile(embed_data['test_find_files/mytestRoot.txt'], contents='')
+        CreateFile(embed_data['test_find_files/A/testA.bmp'], contents='')
+        CreateFile(embed_data['test_find_files/A/mytestA.txt'], contents='')
+        CreateFile(embed_data['test_find_files/A/B/testB.bmp'], contents='')
+        CreateFile(embed_data['test_find_files/A/B/mytestB.txt'], contents='')
+        CreateFile(embed_data['test_find_files/A/C/testC.bmp'], contents='')
+        CreateFile(embed_data['test_find_files/A/C/mytestC.txt'], contents='')
+
+        # no recursion, must return only .bmp files
+        in_filter = ['*.bmp']
+        out_filter = []
+        found_files = list(FindFiles(embed_data['test_find_files'], in_filter, out_filter, False))
+        Compare(found_files, [embed_data['test_find_files/testRoot.bmp']])
+
+        # no recursion, must return all files
+        in_filter = ['*']
+        out_filter = []
+        found_files = list(FindFiles(embed_data['test_find_files'], in_filter, out_filter, False))
+
+        assert_found_files = [
+            embed_data['test_find_files/A'],
+            embed_data['test_find_files/mytestRoot.txt'],
+            embed_data['test_find_files/testRoot.bmp'],
+        ]
+        Compare(found_files, assert_found_files)
+
+        # no recursion, return all files, except *.bmp
+        in_filter = ['*']
+        out_filter = ['*.bmp']
+        found_files = list(FindFiles(embed_data['test_find_files'], in_filter, out_filter, False))
+
+        assert_found_files = [
+            embed_data['test_find_files/A'],
+            embed_data['test_find_files/mytestRoot.txt'],
+        ]
+        Compare(found_files, assert_found_files)
+
+        # recursion, to get just directories
+        in_filter = ['*']
+        out_filter = ['*.bmp', '*.txt']
+        found_files = list(FindFiles(embed_data['test_find_files'], in_filter, out_filter))
+
+        assert_found_files = [
+            embed_data['test_find_files/A'],
+            embed_data['test_find_files/A/B'],
+            embed_data['test_find_files/A/C'],
+        ]
+        Compare(found_files, assert_found_files)
+
+        # recursion with no out_filters, must return all files
+        in_filter = ['*']
+        out_filter = []
+        found_files = list(FindFiles(embed_data['test_find_files']))
+
+        assert_found_files = [
+            embed_data['test_find_files/A'],
+            embed_data['test_find_files/mytestRoot.txt'],
+            embed_data['test_find_files/testRoot.bmp'],
+            embed_data['test_find_files/A/B'],
+            embed_data['test_find_files/A/C'],
+            embed_data['test_find_files/A/mytestA.txt'],
+            embed_data['test_find_files/A/testA.bmp'],
+            embed_data['test_find_files/A/B/mytestB.txt'],
+            embed_data['test_find_files/A/B/testB.bmp'],
+            embed_data['test_find_files/A/C/mytestC.txt'],
+            embed_data['test_find_files/A/C/testC.bmp'],
+        ]
+        assert_found_files = map(PATH, assert_found_files)
+        Compare(found_files, assert_found_files)
+
+        # recursion with no out_filters, must return all files
+        # include_root_dir is False, it will be omitted from the found files
+        in_filter = ['*']
+        out_filter = []
+        found_files = list(FindFiles(embed_data['test_find_files'], include_root_dir=False))
+
+        assert_found_files = [
+            'A',
+            'mytestRoot.txt',
+            'testRoot.bmp',
+            'A/B',
+            'A/C',
+            'A/mytestA.txt',
+            'A/testA.bmp',
+            'A/B/mytestB.txt',
+            'A/B/testB.bmp',
+            'A/C/mytestC.txt',
+            'A/C/testC.bmp',
+        ]
+        assert_found_files = map(PATH, assert_found_files)
+        Compare(found_files, assert_found_files)
+
+        # recursion must return just .txt files
+        in_filter = ['*.txt']
+        out_filter = []
+        found_files = list(FindFiles(embed_data['test_find_files/A'], in_filter, out_filter))
+
+        assert_found_files = [
+            embed_data['test_find_files/A/mytestA.txt'],
+            embed_data['test_find_files/A/B/mytestB.txt'],
+            embed_data['test_find_files/A/C/mytestC.txt'],
+        ]
+        assert_found_files = map(PATH, assert_found_files)
+        Compare(found_files, assert_found_files)
+
+        # recursion must return just .txt files
+        in_filter = ['*.txt']
+        out_filter = ['*A*']
+        found_files = list(FindFiles(embed_data['test_find_files'], in_filter, out_filter))
+
+        assert_found_files = [
+            embed_data['test_find_files/mytestRoot.txt'],
+        ]
+        assert_found_files = map(PATH, assert_found_files)
+        Compare(found_files, assert_found_files)
+
+        # recursion must ignore everyting below a directory that match the out_filter
+        in_filter = ['*']
+        out_filter = ['B', 'C']
+        found_files = list(FindFiles(embed_data['test_find_files'], in_filter, out_filter))
+
+        assert_found_files = [
+            embed_data['test_find_files/A'],
+            embed_data['test_find_files/A/mytestA.txt'],
+            embed_data['test_find_files/A/testA.bmp'],
+            embed_data['test_find_files/mytestRoot.txt'],
+            embed_data['test_find_files/testRoot.bmp'],
+        ]
+        Compare(found_files, assert_found_files)
 
 
     def assertSetEqual(self, a, b):
