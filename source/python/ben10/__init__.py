5d73d60688bb8a71122210ea87a476cbb54e4f67
Guilherme Quentel Melo <guilherme@esss.com.br>
2014-12-03 18:17:39 -0200

    fixup! Code on ben10 __init__ to transform bytes argv to unicode argv on

--- source/python/ben10/__init__.py
+++ source/python/ben10/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+from multiprocessing.process import current_process
+import sys
 #===================================================================================================
 # Placeholder for the "tr" function
 # The "tr" function is implemented by xgui20.translate
@@ -27,35 +29,13 @@ if not hasattr(builtins, 'tr'):
 # communicate this intention
 builtins.builtin_open = open
 
-import sys
-
-def win32_unicode_argv():
-    """Uses shell32.GetCommandLineArgvW to get sys.argv as a list of Unicode
-    strings.
-
-    Versions 2.x of Python don't support Unicode in sys.argv on
-    Windows, with the underlying Windows API instead replacing multi-byte
-    characters with '?'.
-    """
-
-    from ctypes import POINTER, byref, cdll, c_int, windll
-    from ctypes.wintypes import LPCWSTR, LPWSTR
-
-    GetCommandLineW = cdll.kernel32.GetCommandLineW
-    GetCommandLineW.argtypes = []
-    GetCommandLineW.restype = LPCWSTR
-
-    CommandLineToArgvW = windll.shell32.CommandLineToArgvW
-    CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]
-    CommandLineToArgvW.restype = POINTER(LPWSTR)
-
-    cmd = GetCommandLineW()
-    argc = c_int(0)
-    argv = CommandLineToArgvW(cmd, byref(argc))
-    if argc.value > 0:
-        # Remove Python executable and commands if present
-        start = argc.value - len(sys.argv)
-        return [argv[i] for i in
-                xrange(start, argc.value)]
-
-sys.argv = win32_unicode_argv()
+# Versions 2.x of Python don't support Unicode in sys.argv on Windows, with the underlying Windows
+# API instead replacing multi-byte characters with '?'.
+# The unicode argv extracted from windows is valid only for the main process.
+if current_process().name == 'MainProcess':
+    if sys.platform == 'win32':
+        from ben10.unicode_argv import GetWindowsUnicodeArgv
+        sys.argv = GetWindowsUnicodeArgv()
+    else:
+        from ben10.unicode_argv import GetLinuxUnicodeArgv
+        sys.argv = GetLinuxUnicodeArgv()
