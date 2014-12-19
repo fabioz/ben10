4b1b96b30c32b3a9fba782f70b2af4948a843222
Tiago de Holanda Cunha Nobrega <tnobrega@esss.com.br>
2014-12-19 11:44:17 -0200

    Revert "Merge remote-tracking branch 'origin/rb-ROCKY-v3.2.3' into fb-ROCKY-1288-unicode-master"
    This reverts commit cef982e3ebce0711f4491355a51d6a87ceafaf1f, reversing
    changes made to d86d21e0fcd1652775e65ba8d2845ee4e4e73ddf.

--- source/python/ben10/__init__.py
+++ source/python/ben10/__init__.py
@@ -1,6 +1,4 @@
 from __future__ import unicode_literals
-from multiprocessing.process import current_process
-import sys
 #===================================================================================================
 # Placeholder for the "tr" function
 # The "tr" function is implemented by xgui20.translate
@@ -28,14 +26,3 @@ if not hasattr(builtins, 'tr'):
 # object to C++, which won't work with the wrapper object returned by io.open) use this to
 # communicate this intention
 builtins.builtin_open = open
-
-# Versions 2.x of Python don't support Unicode in sys.argv on Windows, with the underlying Windows
-# API instead replacing multi-byte characters with '?'.
-# The unicode argv extracted from windows is valid only for the main process.
-if current_process().name == 'MainProcess':
-    if sys.platform == 'win32':
-        from ben10.unicode_argv import GetWindowsUnicodeArgv
-        sys.argvu = GetWindowsUnicodeArgv()
-    else:
-        from ben10.unicode_argv import GetLinuxUnicodeArgv
-        sys.argvu = GetLinuxUnicodeArgv()
