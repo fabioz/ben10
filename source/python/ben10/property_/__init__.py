b007a28c2badb4941d754a32e1e4982f7f26643a
Alexandre Andrade <ama@esss.com.br>
2015-02-09 22:30:46 -0200

    Improving coverage for ben10.property_.

--- source/python/ben10/property_/__init__.py
+++ source/python/ben10/property_/__init__.py
@@ -3,4 +3,4 @@ from ._properties_descriptor import PropertiesDescriptor, PropertiesDict
 from ._property import Property
 from ._property_create import Create, CreateDeprecatedProperties, CreateForwardProperties
 from ._property_naming import FromCamelCase, MakeGetName, MakeSetGetName, MakeSetName, ToCamelCase
-from ._property_operators import Copy, DeepCopy, Eq, PropertiesStr
+from ._property_operators import Copy, DeepCopy, Eq
