d805e3546a9f8ce9171b84890c47c02655451c3e
Diogo de Campos <campos@esss.com.br>
2015-09-08 09:33:20 -0300

    Creating constants_generated for ben10

--- scripts/ben10_install.py
+++ scripts/ben10_install.py
@@ -13,6 +13,7 @@ class Ben10InstallCommand(BaseInstallCommand):
 
     @Override(BaseInstallCommand.Install)
     def Install(self, command_line_args):
+        self.InstallConstantsGenerated()
         self.InstallCog('`self.python_dir`/ben10')
 
 
