6fac7cb55179aad851637932797c18b3227bb7c6
Edison Gustavo Muenz <muenz@esss.com.br>
2015-09-09 13:13:23 -0300

    StripDebugRefs() was moved to ben10

--- source/python/ben10/debug/__init__.py
+++ source/python/ben10/debug/__init__.py
@@ -1,4 +1,7 @@
 from __future__ import unicode_literals
+
+
+
 def IsPythonDebug():
     '''
     Returns True if it is running under a debug version of the python interpreter
@@ -7,3 +10,28 @@ def IsPythonDebug():
     import sys
 
     return hasattr(sys, 'gettotalrefcount')  # Only exists in debug versions
+
+
+def StripDebugRefs(text, end_only=True):
+    """
+    Strips the ref count text that is printed by the debug version of the python interpreter after
+    program exit ("[1209 refs]" for example).
+
+    Should be used by tests that capture output from a debug python interpreter, because it will
+    print this characters resulting in different output when compared to the output produced
+    by the release version of the interpreter.
+
+    :param str text: text to strip debug information
+
+    :param bool end_only: if True, only strips debug info from the end of the string, otherwise will
+        search and strip the entire string from debug references.
+
+    :rtype: str
+    :return: stripped text
+    """
+    if IsPythonDebug():
+        pattern = r'(\[\d+ refs\]\n?)'
+        if end_only:
+            pattern += '$'
+        return re.sub(pattern, '', text)
+    return text
